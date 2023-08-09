49e03e44211cfb5614395bdd32872bb8f8937df4
alexcoman1 <octavianalexandru.coman@zebra.com>
2023-08-09 12:55:12 +0200

    update

--- index.js
+++ index.js
@@ -15,11 +15,7 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({extended: false}))
-app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
 
 app.use('/', require('./routes/authRoutes'))
 
